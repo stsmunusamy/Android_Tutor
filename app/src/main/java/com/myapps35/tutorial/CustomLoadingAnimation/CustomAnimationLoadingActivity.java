8940cbab7bddb9609540bee31f20d5b2b713c3ac
stsMunusamy <git+stsdevarajan@spanenterprises.com>
2016-06-22 16:39:44 +0530

    custom progress bar is created in separate activity

--- app/src/main/java/com/myapps35/tutorial/CustomLoadingAnimation/CustomAnimationLoadingActivity.java
+++ app/src/main/java/com/myapps35/tutorial/CustomLoadingAnimation/CustomAnimationLoadingActivity.java
@@ -14,34 +14,15 @@ public class CustomAnimationLoadingActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_animation_loading);
-
-        while (true)
-        {
-            try
-            {
-                startAnim();
-
-                Thread.sleep(5000);
-
-                stopAnim();
-
-                Thread.sleep(2000);
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
     }
 
     void startAnim()
     {
-        findViewById(R.id.avloadingIndicatorView).setVisibility(View.VISIBLE);
+        findViewById(R.id.SemiCircleSpin).setVisibility(View.VISIBLE);
     }
 
     void stopAnim()
     {
-        findViewById(R.id.avloadingIndicatorView).setVisibility(View.GONE);
+        findViewById(R.id.SemiCircleSpin).setVisibility(View.GONE);
     }
 }
