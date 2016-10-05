65ad399319be00871c4ebe4978e881a0394a77e9
stsMunusamy <git+stsdevarajan@spanenterprises.com>
2016-10-05 18:33:03 +0530

    New Branch Created for AndroidExpriment Projects

---
+++ app/src/main/java/com/myapps35/tutorial/AndroidExpriment/AndroidExprimentActivity.java
@@ -0,0 +1,17 @@
+package com.myapps35.tutorial.AndroidExpriment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.myapps35.tutorial.R;
+
+public class AndroidExprimentActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_expriment);
+    }
+}
