a90e1c640575dc09d83adece992c137a553e2113
stsMunusamy <git+stsdevarajan@spanenterprises.com>
2016-10-24 10:12:34 +0530

    File Sharing Via Intent Activity Created

--- app/src/main/java/com/myapps35/tutorial/Facebook_Integration/FacebookSinginActivity.java
+++ app/src/main/java/com/myapps35/tutorial/Facebook_Integration/FacebookSinginActivity.java
@@ -3,16 +3,24 @@ package com.myapps35.tutorial.Facebook_Integration;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.myapps35.tutorial.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -36,21 +44,48 @@ public class FacebookSinginActivity extends Activity
 
         loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday"));
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
             @Override
-            public void onSuccess(LoginResult loginResult) {
+            public void onSuccess(LoginResult loginResult)
+            {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
 
 
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
+                {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response)
+                    {
+                        Log.v("LoginScreenActivity", response.toString());
+
+                        try
+                        {
+
+                        }
+                        catch (Exception e)
+                        {
+
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel()
+            {
                 // App code
             }
 
             @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
+            public void onError(FacebookException exception)
+            {
                 // App code
             }
         });
@@ -58,7 +93,8 @@ public class FacebookSinginActivity extends Activity
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
 
         super.onActivityResult(requestCode, resultCode, data);
 
---
+++ app/src/main/java/com/myapps35/tutorial/FileSharingViaIntent/FileSharingActivity.java
@@ -0,0 +1,17 @@
+package com.myapps35.tutorial.FileSharingViaIntent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.myapps35.tutorial.R;
+
+public class FileSharingActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_sharing);
+    }
+}
