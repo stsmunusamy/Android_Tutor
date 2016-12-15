6c4fd0f51284e6de5329d6e02936ec923730b3f9
stsMunusamy <git+stsdevarajan@spanenterprises.com>
2016-12-15 16:20:03 +0530

    Recycler View is created

---
+++ app/src/main/java/com/myapps35/tutorial/RecyclerView/Adapter/MyRecyclerAdapter.java
@@ -0,0 +1,83 @@
+package com.myapps35.tutorial.RecyclerView.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.myapps35.tutorial.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by span-tech on 12/15/2016.
+ */
+
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder>
+{
+
+    ArrayList<String> mDataList;
+
+
+    /*
+    *  Step 1:
+    *  Get the Data List for Create Views
+    */
+    public MyRecyclerAdapter(ArrayList<String> mDataList)
+    {
+        this.mDataList = mDataList;
+
+    }
+
+    /*
+    *  Step 3:
+    *  Initialize the Layout & return the view
+    */
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mrecycler_view_adapter_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    /*
+    *  Step 5:
+    *  Set the data's for view items with respective position & viewHolder
+    */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position)
+    {
+
+    }
+
+    /*
+   *  Step 2:
+   *  Initialize the Data List size for Create the no of view with repect to Data's
+   */
+    @Override
+    public int getItemCount()
+    {
+        return mDataList.size();
+    }
+
+    /*
+    *  Step 4:
+    *  Declare & Initialize the View Items
+    */
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView lblDescription;
+        TextView lblName;
+
+        ViewHolder(View itemView)
+        {
+            super(itemView);
+
+            lblName = (TextView) itemView.findViewById(R.id.lblName);
+            lblDescription = (TextView) itemView.findViewById(R.id.lblDescription);
+
+        }
+    }
+}
--- app/src/main/java/com/myapps35/tutorial/RecyclerView/Controller/RecyclerDemoActivity.java
+++ app/src/main/java/com/myapps35/tutorial/RecyclerView/Controller/RecyclerDemoActivity.java
@@ -2,16 +2,74 @@ package com.myapps35.tutorial.RecyclerView.Controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.myapps35.tutorial.R;
+import com.myapps35.tutorial.RecyclerView.Adapter.MyRecyclerAdapter;
+
+import java.util.ArrayList;
 
 public class RecyclerDemoActivity extends Activity
 {
+
+    /*
+    *  Step 1:
+    *  Declare 3 components for Recycler View
+    *  Widget
+    *  Adater
+    *  Layout Mangaer
+    */
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mRecyclerAdapter;
+    RecyclerView.LayoutManager mRecyclerLayoutManager;
+
+    ArrayList<String> mDataList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_demo);
+
+        initialize();
+
+        setAdapter();
+    }
+
+    private void initialize()
+    {
+        /*
+        *  Step 2:
+        *  Initialize the View
+        */
+        mRecyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
+    }
+
+    private void setAdapter()
+    {
+        /*
+        *  Step 3:
+        *  Use this setting to improve performance if you know that changes
+        *  in content do not change the layout size of the RecyclerView
+        */
+        mRecyclerView.setHasFixedSize(true);
+
+
+        /*
+        *  Step 4:
+        *  set the Linear Layout Properties for RecyclerView
+        */
+        mRecyclerLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mRecyclerLayoutManager);
+
+        /*
+        *  Step 5:
+        *  Initialize & set the adapter for RecyclerView
+        */
+        mRecyclerAdapter = new MyRecyclerAdapter(mDataList);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
     }
 }
