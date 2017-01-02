f2fc223c6bf2bc4094bff2a90fec3f8c941e7c49
stsMunusamy <git+stsdevarajan@spanenterprises.com>
2017-01-02 10:17:17 +0530

    New Branch was created for styling

---
+++ app/src/main/java/com/myapps35/tutorial/ETE8868/PayerRecipientActivity.java
@@ -0,0 +1,17 @@
+package com.myapps35.tutorial.ETE8868;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.myapps35.tutorial.R;
+
+public class PayerRecipientActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payer_recipient);
+    }
+}
